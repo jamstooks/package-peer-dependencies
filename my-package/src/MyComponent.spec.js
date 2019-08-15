69efc98e9563a972531161119c5f689f8f1d84bb
Benjamin Stookey <ben.stookey@gmail.com>
2019-08-15 14:01:10 -0700

    fixed tests for everything-is-broken example

--- my-package/src/MyComponent.spec.js
+++ my-package/src/MyComponent.spec.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import { MyComponent } from "my-package";
+import { MyComponent } from "./MyComponent";
 
 describe("MyComponent", () => {
   it("Renders w/out errors", () => {
